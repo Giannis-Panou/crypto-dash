884c390c86ede682a1be12b72169f7acb147ccb9
Giannis Panou <giannispanou500@gmail.com>
2025-08-17 14:12:52 +0300

    Release

---
+++ src/pages/about.jsx
@@ -0,0 +1,28 @@
+const AboutPage = () => {
+	return (
+		<div className='about'>
+			<h1>About Crypto Dash</h1>
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas ratione
+				officiis iusto sed sunt eum magni deserunt dolor laborum voluptatum
+				repudiandae quia, a, corrupti, enim blanditiis qui dolorum rerum
+				tempore.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio eaque
+				perspiciatis incidunt alias harum. Ex culpa quibusdam nostrum non? Eos
+				eveniet iure aperiam animi quia. Rerum dignissimos minima impedit.
+				Molestias, aperiam amet. Hic consectetur totam itaque vel voluptates
+				tempore quas!
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum nulla
+				dignissimos necessitatibus enim delectus magni nobis magnam error
+				exercitationem soluta minus, excepturi, odio porro voluptatibus at
+				ducimus nemo quasi nihil.
+			</p>
+		</div>
+	);
+};
+
+export default AboutPage;
